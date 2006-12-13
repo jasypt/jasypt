357ac4b139404562e35cd853ac456b548a2e4ea0
Daniel Fernández <daniel.fernandez@11thlabs.org>
2006-12-13 18:58:21 +0000

    Added unit test

---
+++ jasypt/src/test/java/org/jasypt/encryption/PasswordBasedEncryptorTest.java
@@ -0,0 +1,23 @@
+/*
+ * @(#)PasswordBasedEncryptorTest.java   $Revision: 1.1 $ $Date: 2006-12-13 18:58:21 $
+ *
+ * Copyright (c) 2.006, denodo technologies, S.L. All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * denodo technologies, S.L. ("Confidential Information"). You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with denodo technologies S.L.
+ */
+package org.jasypt.encryption;
+
+import junit.framework.TestCase;
+
+public class PasswordBasedEncryptorTest extends TestCase {
+
+    public void testEncryptAndDecrypt() {
+        
+        
+    }
+
+}
