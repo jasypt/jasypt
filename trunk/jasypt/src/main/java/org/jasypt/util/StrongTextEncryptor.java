2403d4fbee735ab5acd6c19f0d68ca1acc38eeb5
dfernandez <dfernandez@da78853a-38a2-49c0-9af2-345bcb92683f>
2006-12-20 15:05:37 +0000

    Removed automatic method comments

--- trunk/jasypt/src/main/java/org/jasypt/util/StrongTextEncryptor.java
+++ trunk/jasypt/src/main/java/org/jasypt/util/StrongTextEncryptor.java
@@ -12,24 +12,15 @@ public final class StrongTextEncryptor implements TextEncryptor {
         this.encryptor = new PBEWithMD5AndTripleDESStringEncryptor();
     }
     
-    /* (non-Javadoc)
-     * @see org.jasypt.util.TextEncryptor#setPassword(java.lang.String)
-     */
     public synchronized void setPassword(String password) {
         encryptor.setPassword(password);
     }
 
     
-    /* (non-Javadoc)
-     * @see org.jasypt.util.TextEncryptor#encrypt(java.lang.String)
-     */
     public synchronized String encrypt(String message) {
         return encryptor.encrypt(message);
     }
     
-    /* (non-Javadoc)
-     * @see org.jasypt.util.TextEncryptor#decrypt(java.lang.String)
-     */
     public synchronized String decrypt(String encryptedMessage) {
         return encryptor.decrypt(encryptedMessage);
     }
