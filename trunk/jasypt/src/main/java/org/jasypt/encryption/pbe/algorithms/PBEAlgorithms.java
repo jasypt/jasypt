a603e9a1d9c0c6fe4f5e7dbdf8dbd4db67cd81ba
dfernandez <dfernandez@da78853a-38a2-49c0-9af2-345bcb92683f>
2007-01-29 01:07:55 +0000

    Added javadoc

--- trunk/jasypt/src/main/java/org/jasypt/encryption/pbe/algorithms/PBEAlgorithms.java
+++ trunk/jasypt/src/main/java/org/jasypt/encryption/pbe/algorithms/PBEAlgorithms.java
@@ -27,6 +27,29 @@ import java.util.Map;
 
 import org.jasypt.exceptions.EncryptionInitializationException;
 
+/**
+ * <p>
+ * Registry for all the allowed Password Based Encryption algorithms.
+ * </p>
+ * <p>
+ * These are:
+ * <ul>
+ *   <li><b><tt>PBEWithMD5AndDES</tt></b></li>
+ *   <li><b><tt>PBEWithMD5AndTripleDES</tt></b> (you may need to download the 
+ *       <a href="http://java.sun.com/javase/downloads" target="_blank"><i>Java
+ *       Cryptography 
+ *       Extension (JCE) Unlimited Strength Jurisdiction Policy Files</i></a>
+ *       to use this algorithm).</li>
+ *   <li><b><tt>PBEWithSHA1AndDESede</tt></b></li>
+ *   <li><b><tt>PBEWithSHA1AndRC2_40</tt></b></li>
+ * </ul>
+ * </p>
+ * 
+ * @since 1.0
+ * 
+ * @author Daniel Fern&aacute;ndez Garrido
+ * 
+ */
 public class PBEAlgorithms {
 
     public static final String PBE_WITH_MD5_AND_DES = 
@@ -63,18 +86,51 @@ public class PBEAlgorithms {
         parametersByAlgorithm.put(parameters.getName(), parameters);
     }
     
-    
+
+    /**
+     * <p>
+     * Checks that an algorithm name coming from configuration is a valid one.
+     * </p>
+     * <p>
+     * <i>This method is intended for internal use only.</i>
+     * </p>
+     * 
+     * @param algorithmName the name to be checked.
+     * @throws EncryptionInitializationException if the algorithm name is not
+     *         a valid one.
+     */
     public static void validateAlgorithm(String algorithmName) {
         if (getParameters(algorithmName) == null) {
             throw new EncryptionInitializationException(algorithmName +
                     " is not a supported algorithm name");
         }
     }
+
     
+    /**
+     * <p>
+     * Returns the parameter values associated with a specific algorithm.
+     * </p>
+     * <p>
+     * <i>This method is intended for internal use only.</i>
+     * </p>
+     * 
+     * @param algorithmName the name of the algorithm
+     * @return the associated parameters
+     */
     public static Parameters getParameters(String algorithmName) {
         return (Parameters) parametersByAlgorithm.get(algorithmName);
     }
     
+    
+    /**
+     * <p>
+     * Returns the array of valid (registered) algorithm names which can be
+     * used for Password Based Encryption.
+     * </p>
+     * 
+     * @return the algorithm names as an array of Strings
+     */
     public static String[] getSupportedAlgorithms() {
         List algorithmNames = new ArrayList(parametersByAlgorithm.keySet());
         Collections.sort(algorithmNames);
@@ -82,7 +138,20 @@ public class PBEAlgorithms {
                 algorithmNames.toArray(new String[algorithmNames.size()]);
     }
     
-    
+
+    /**
+     * <p>
+     * Internal class which models the parameters associated with a specific
+     * PBE algorithm.
+     * </p> 
+     * <p>
+     * <i>This class is intended for internal use only.</i>
+     * </p>
+     * 
+     * @since 1.0
+     * 
+     * @author Daniel Fern&aacute;ndez Garrido
+     */
     public static class Parameters {
         
         private String name = null;
