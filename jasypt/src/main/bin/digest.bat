a48a2bbcb4a39d7cc2cb401a66293fffd96b48c7
Daniel Fernández <daniel.fernandez@11thlabs.org>
2007-11-19 15:05:41 +0000

    Several changes made

--- jasypt/src/main/bin/digest.bat
+++ jasypt/src/main/bin/digest.bat
@@ -4,12 +4,12 @@ IF "%OS%" == "Windows_NT" setlocal
 set SCRIPT_NAME=digest
 set EXECUTABLE_CLASS=org.jasypt.intf.cli.JasyptStringDigestCLI
 set CURRENT_DIR=%cd%
-set CLASSPATH=.;%CLASSPATH%;%CURRENT_DIR%\jasypt-cli-bundle.jar
+set EXEC_CLASSPATH=.;%CURRENT_DIR%\jasypt-cli-bundle.jar;%JASYPT_CLASSPATH%
 
 set JAVA_EXECUTABLE=java
 if "%JAVA_HOME%" == "" goto execute
 set JAVA_EXECUTABLE="%JAVA_HOME%\bin\java"
 
 :execute
-%JAVA_EXECUTABLE% -classpath %CLASSPATH% %EXECUTABLE_CLASS% %SCRIPT_NAME% %*
+%JAVA_EXECUTABLE% -classpath %EXEC_CLASSPATH% %EXECUTABLE_CLASS% %SCRIPT_NAME% %*
 
