59c2ad81f90047e41f1b20a97db2d8cee09e50a4
dfernandez <dfernandez@da78853a-38a2-49c0-9af2-345bcb92683f>
2007-11-19 14:42:36 +0000

    Several changes made

--- trunk/jasypt/src/main/bin/decrypt.bat
+++ trunk/jasypt/src/main/bin/decrypt.bat
@@ -4,12 +4,12 @@ IF "%OS%" == "Windows_NT" setlocal
 set SCRIPT_NAME=decrypt
 set EXECUTABLE_CLASS=org.jasypt.intf.cli.JasyptPBEStringDecryptionCLI
 set CURRENT_DIR=%cd%
-set CLASSPATH=%CURRENT_DIR%\jasypt-cli-bundle.jar
+set CLASSPATH=.;%CLASSPATH%;%CURRENT_DIR%\jasypt-cli-bundle.jar
 
 set JAVA_EXECUTABLE=java
 if "%JAVA_HOME%" == "" goto execute
 set JAVA_EXECUTABLE="%JAVA_HOME%\bin\java"
 
 :execute
-%JAVA_EXECUTABLE% %EXECUTABLE_CLASS% %SCRIPT_NAME% %*
+%JAVA_EXECUTABLE% -classpath %CLASSPATH% %EXECUTABLE_CLASS% %SCRIPT_NAME% %*
 
