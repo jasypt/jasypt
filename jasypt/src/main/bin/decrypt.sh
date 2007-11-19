3c8241bcd3768b874f8efa58cc20fd1812e1e833
Daniel Fernández <daniel.fernandez@11thlabs.org>
2007-11-19 14:42:36 +0000

    Several changes made

--- jasypt/src/main/bin/decrypt.sh
+++ jasypt/src/main/bin/decrypt.sh
@@ -3,13 +3,13 @@
 SCRIPT_NAME=decrypt
 EXECUTABLE_CLASS=org.jasypt.intf.cli.JasyptPBEStringDecryptionCLI
 CURRENT_DIR=$PWD
-CLASSPATH=$CURRENT_DIR/jasypt-cli-bundle.jar
+CLASSPATH=.:$CLASSPATH:$CURRENT_DIR/jasypt-cli-bundle.jar
 
 JAVA_EXECUTABLE=java
-if [ $JAVA_HOME = "" ];
+if [ "$JAVA_HOME" != "" ]
 then
   $JAVA_EXECUTABLE=$JAVA_HOME/bin/java
 fi
 
-$JAVA_EXECUTABLE $EXECUTABLE_CLASS $SCRIPT_NAME $@
+$JAVA_EXECUTABLE -classpath $CLASSPATH $EXECUTABLE_CLASS $SCRIPT_NAME $@
 
