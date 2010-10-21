c3b6e0d8a11666614dd8e890b61dfc2abea08eff
dfernandez <dfernandez@da78853a-38a2-49c0-9af2-345bcb92683f>
2010-10-21 22:19:34 +0000

    Several changes made

--- trunk/jasypt/src/main/bin/decrypt.bat
+++ trunk/jasypt/src/main/bin/decrypt.bat
@@ -6,6 +6,10 @@ cd %0\..
 cd ..
 set EXECUTABLE_CLASS=org.jasypt.intf.cli.JasyptPBEStringDecryptionCLI
 set EXEC_CLASSPATH=.
+if "%JASYPT_CLASSPATH%" == "" goto computeclasspath
+set EXEC_CLASSPATH=%EXEC_CLASSPATH%;%JASYPT_CLASSPATH%
+
+:computeclasspath
 FOR %%c in (.\lib\*.jar) DO set EXEC_CLASSPATH=!EXEC_CLASSPATH!;%%c
 
 set JAVA_EXECUTABLE=java
@@ -13,4 +17,5 @@ if "%JAVA_HOME%" == "" goto execute
 set JAVA_EXECUTABLE="%JAVA_HOME%\bin\java"
 
 :execute
+echo Using classpath: %EXEC_CLASSPATH%
 %JAVA_EXECUTABLE% -classpath %EXEC_CLASSPATH% %EXECUTABLE_CLASS% %SCRIPT_NAME% %*
--- trunk/jasypt/src/main/bin/listAlgorithms.bat
+++ trunk/jasypt/src/main/bin/listAlgorithms.bat
@@ -6,6 +6,10 @@ cd %0\..
 cd ..
 set EXECUTABLE_CLASS=org.jasypt.intf.cli.AlgorithmRegistryCLI
 set EXEC_CLASSPATH=.
+if "%JASYPT_CLASSPATH%" == "" goto computeclasspath
+set EXEC_CLASSPATH=%EXEC_CLASSPATH%;%JASYPT_CLASSPATH%
+
+:computeclasspath
 FOR %%c in (.\lib\*.jar) DO set EXEC_CLASSPATH=!EXEC_CLASSPATH!;%%c
 
 set JAVA_EXECUTABLE=java
