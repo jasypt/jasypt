7755182a09f93286a6d231d9d5853f857101aa6b
Daniel Fernández <daniel.fernandez@11thlabs.org>
2011-12-04 17:59:58 +0000

    Several changes made

---
+++ jasypt-wicket13/src/main/bin/decrypt.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+SCRIPT_NAME=decrypt.sh
+EXECUTABLE_CLASS=org.jasypt.intf.cli.JasyptPBEStringDecryptionCLI
+BIN_DIR=`dirname $0`
+DIST_DIR=$BIN_DIR/..
+LIB_DIR=$DIST_DIR/lib
+EXEC_CLASSPATH="."
+
+if [ -n "$JASYPT_CLASSPATH" ]
+then
+  EXEC_CLASSPATH=$EXEC_CLASSPATH:$JASYPT_CLASSPATH
+fi
+
+for a in `find $LIB_DIR -name '*.jar'`
+do
+  EXEC_CLASSPATH=$EXEC_CLASSPATH:$a
+done
+
+JAVA_EXECUTABLE=java
+if [ -n "$JAVA_HOME" ]
+then
+  JAVA_EXECUTABLE=$JAVA_HOME/bin/java
+fi
+
+$JAVA_EXECUTABLE -classpath $EXEC_CLASSPATH $EXECUTABLE_CLASS $SCRIPT_NAME "$@"
