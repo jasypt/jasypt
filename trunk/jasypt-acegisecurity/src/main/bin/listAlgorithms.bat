0774be7548bea975cd684dc5592e6cb26f6cf512
dfernandez <dfernandez@da78853a-38a2-49c0-9af2-345bcb92683f>
2011-12-06 00:19:53 +0000

    Share project "jasypt-acegisecurity" into "https://jasypt.svn.sourceforge.net/svnroot/jasypt"

---
+++ trunk/jasypt-acegisecurity/src/main/bin/listAlgorithms.bat
@@ -0,0 +1,21 @@
+@ECHO OFF
+
+set SCRIPT_NAME=listAlgorithms.bat
+cd %0\..
+cd ..
+set EXECUTABLE_CLASS=org.jasypt.intf.cli.AlgorithmRegistryCLI
+set EXEC_CLASSPATH=.
+if "%JASYPT_CLASSPATH%" == "" goto computeclasspath
+set EXEC_CLASSPATH=%EXEC_CLASSPATH%;%JASYPT_CLASSPATH%
+
+:computeclasspath
+IF "%OS%" == "Windows_NT" setlocal ENABLEDELAYEDEXPANSION
+FOR %%c in (.\lib\*.jar) DO set EXEC_CLASSPATH=!EXEC_CLASSPATH!;%%c
+IF "%OS%" == "Windows_NT" setlocal DISABLEDELAYEDEXPANSION
+
+set JAVA_EXECUTABLE=java
+if "%JAVA_HOME%" == "" goto execute
+set JAVA_EXECUTABLE="%JAVA_HOME%\bin\java"
+
+:execute
+%JAVA_EXECUTABLE% -classpath %EXEC_CLASSPATH% %EXECUTABLE_CLASS% %SCRIPT_NAME% %*
