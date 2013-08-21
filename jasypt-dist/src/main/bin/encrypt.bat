082770e4c5cdc31bc8f6334134cb216b1b736ef9
Daniel Fernández <daniel.fernandez@11thlabs.org>
2013-08-21 21:09:46 +0000

    Applied sureshnath path on script files.

--- jasypt-dist/src/main/bin/encrypt.bat
+++ jasypt-dist/src/main/bin/encrypt.bat
@@ -1,8 +1,6 @@
 @ECHO OFF
 
 set SCRIPT_NAME=encrypt.bat
-cd %0\..
-cd ..
 set EXECUTABLE_CLASS=org.jasypt.intf.cli.JasyptPBEStringEncryptionCLI
 set EXEC_CLASSPATH=.
 if "%JASYPT_CLASSPATH%" == "" goto computeclasspath
@@ -10,7 +8,7 @@ set EXEC_CLASSPATH=%EXEC_CLASSPATH%;%JASYPT_CLASSPATH%
 
 :computeclasspath
 IF "%OS%" == "Windows_NT" setlocal ENABLEDELAYEDEXPANSION
-FOR %%c in (.\lib\*.jar) DO set EXEC_CLASSPATH=!EXEC_CLASSPATH!;%%c
+FOR %%c in (%~dp0..\lib\*.jar) DO set EXEC_CLASSPATH=!EXEC_CLASSPATH!;%%c
 IF "%OS%" == "Windows_NT" setlocal DISABLEDELAYEDEXPANSION
 
 set JAVA_EXECUTABLE=java
